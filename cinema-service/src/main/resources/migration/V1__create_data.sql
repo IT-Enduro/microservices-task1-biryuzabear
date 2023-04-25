ac7e81e42de2525505f7e9bbcf2b78a7273f9aa3
Vladimir Fedorchenko <biryuzabear@gmail.com>
2023-04-26 01:56:11 +0400

    Добавлена миграция

---
+++ cinema-service/src/main/resources/migration/V1__create_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO cinema (id, cinema_uid, name, address) VALUES (1, '06cc4ba3-ee97-4d29-a814-c40588290d17', 'Кинотеатр Москва', 'Ереван, улица Хачатура Абовяна, 18');
+INSERT INTO film_session (id, cinema_id, film_uid, date, total_seats, booked_seats) VALUES (1, 1, '049161bb-badd-4fa8-9d90-87c9a82b0668', '2024-01-01 08:00:00', 5000, 0);
