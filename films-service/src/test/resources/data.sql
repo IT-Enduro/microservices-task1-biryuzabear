6943fc7a0faceb001bd0230409bffe49d5ee155f
Vladimir Fedorchenko <biryuzabear@gmail.com>
2023-04-25 04:33:08 +0400

    надо добавить данные и поменять остальные ссылки на внутренние для сети контейнеров

---
+++ films-service/src/test/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO film (id, film_uid, name, rating, director, producer, genre) VALUES (1, '049161bb-badd-4fa8-9d90-87c9a82b0668', 'Terminator 2 Judgment day', 8.6, 'James Cameron', 'James Cameron', 'Sci-Fi');
+
